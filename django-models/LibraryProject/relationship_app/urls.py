d66b3006c2dc859b0c3cec09b4986be74910501e
TeiderseleshI <redietseleshii@gmail.com>
2025-07-16 11:52:24 +0300

    Add URL patterns in relationship_app/urls.py for user registration, login, and logout views with template assignments

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register_view, CustomLoginView, CustomLogoutView
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register_view, name='register'),
+    path('register/', views.register, name='register'),  
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
