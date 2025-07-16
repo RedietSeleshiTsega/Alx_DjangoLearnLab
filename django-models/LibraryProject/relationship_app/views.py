3aeaed29627b26d84b964df854e1cdf321099413
TeiderseleshI <redietseleshii@gmail.com>
2025-07-16 12:05:25 +0300

    Add permission_required decorators to book management views in relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,40 +1,35 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  
 
-def register(request):
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = BookForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('admin_view')  
+            form.save()
+            return redirect('book_list')  
     else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', pk=book.pk)  
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
-def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
-
-
-@login_required
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list') 
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
