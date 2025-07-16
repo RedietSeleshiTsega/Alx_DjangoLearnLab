5f68bbf3c662d989f5a655e89dccaee600e899c8
TeiderseleshI <redietseleshii@gmail.com>
2025-07-16 11:50:36 +0300

    Add missing import for UserCreationForm in relationship\_app/views.py to enable user registration functionality.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,8 @@
-
-
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+
 
 
 def register_view(request):
