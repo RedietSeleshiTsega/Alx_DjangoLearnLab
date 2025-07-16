8b86bafb4d9f4f5fd917466e58edd750fcde3b61
TeiderseleshI <redietseleshii@gmail.com>
2025-07-16 11:27:47 +0300

    fix: update author book query to use filter for checker compatibility.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,10 @@
-
-
 from relationship_app.models import Author, Book, Library, Librarian
 
 
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        return author.books.all()
+        return Book.objects.filter(author=author) 
     except Author.DoesNotExist:
         return []
 
@@ -23,7 +21,5 @@ def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
         return library.librarian  
-    except Library.DoesNotExist:
-        return None
-    except Librarian.DoesNotExist:
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
