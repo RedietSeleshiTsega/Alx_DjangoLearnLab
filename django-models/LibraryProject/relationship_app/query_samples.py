07970e17c0644e63a8a07027a03c0ee4887a01d7
Rediet Seleshi Tsega <122819068+RedietSeleshiTsega@users.noreply.github.com>
2025-07-21 23:25:17 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,25 +1,24 @@
+import django
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
+django.setup()
+
 from relationship_app.models import Author, Book, Library, Librarian
 
+def queries():
+    author = Author.objects.get(name="Chinua Achebe")
+    print("Books by Chinua Achebe:")
+    for book in Book.objects.filter(author=author):
+        print(book.title)
 
-def get_books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return Book.objects.filter(author=author)
-    except Author.DoesNotExist:
-        return []
+    library = Library.objects.get(name="Main Library")
+    print("\nBooks in Main Library:")
+    for book in library.books.all():
+        print(book.title)
 
+    librarian = Librarian.objects.get(library=library)
+    print(f"\nLibrarian for Main Library: {librarian.name}")
 
-def get_books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
-
-
-def get_librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return Librarian.objects.get(library=library)  
-    except (Library.DoesNotExist, Librarian.DoesNotExist):
-        return None
+if __name__ == "__main__":
+    queries()
