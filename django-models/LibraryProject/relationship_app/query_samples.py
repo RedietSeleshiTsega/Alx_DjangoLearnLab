9057c97aca071b4730741814ff40d513fe5f42df
Rediet Seleshi Tsega <122819068+RedietSeleshiTsega@users.noreply.github.com>
2025-07-21 23:36:01 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,18 +7,23 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 def queries():
-    author = Author.objects.get(name="Chinua Achebe")
-    print("Books by Chinua Achebe:")
+    
+    author_name = "Chinua Achebe"
+    author = Author.objects.get(name=author_name)
+    print(f"Books by {author.name}:")
     for book in Book.objects.filter(author=author):
         print(book.title)
 
-    library = Library.objects.get(name="Main Library")
-    print("\nBooks in Main Library:")
+    
+    library_name = "Main Library"
+    library = Library.objects.get(name=library_name)
+    print(f"\nBooks in {library.name}:")
     for book in library.books.all():
         print(book.title)
 
+   
     librarian = Librarian.objects.get(library=library)
-    print(f"\nLibrarian for Main Library: {librarian.name}")
+    print(f"\nLibrarian for {library.name}: {librarian.name}")
 
 if __name__ == "__main__":
     queries()
