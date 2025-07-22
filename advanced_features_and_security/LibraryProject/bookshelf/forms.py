bb890e59793e642a790ed310960d8d2ce3c7d96f
TeiderseleshI <redietseleshii@gmail.com>
2025-07-22 12:57:39 +0300

    Made some changes.

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -3,8 +3,12 @@ from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=False, strip=True)
+    
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100)
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date', 'isbn']
+        
