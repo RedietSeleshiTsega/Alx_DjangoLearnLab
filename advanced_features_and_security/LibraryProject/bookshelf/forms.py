dcf95c9d2c20202169870227ba0ace3a562233b1
TeiderseleshI <redietseleshii@gmail.com>
2025-07-22 12:53:09 +0300

    updated some flies.

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import Book
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=False, strip=True)
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date', 'isbn']
