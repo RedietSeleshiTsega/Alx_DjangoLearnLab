6df3d7fdf1882403871dc19268fffea9321ef06c
TeiderseleshI <redietseleshii@gmail.com>
2025-07-22 13:04:46 +0300

    Made some changes.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 from .forms import BookForm
 from .forms import BookSearchForm
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
