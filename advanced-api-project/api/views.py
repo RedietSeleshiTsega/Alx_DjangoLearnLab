9d11c44d3d4870414516546235feaf2633e7687d
Rediet Tsega <redietseleshii@gmail.com>
2025-08-09 11:45:59 +0300

    Updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,20 +1,21 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated  
-from rest_framework.permissions import AllowAny, IsAdminUser  
+from rest_framework import generics, filters
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAuthenticated,
+    AllowAny,
+    IsAdminUser
+)
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import BookFilter
-from django_filters import rest_framework
-from rest_framework.filters import OrderingFilter, SearchFilter 
 
 class BookListView(generics.ListAPIView):
     """List all books (GET)"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = BookFilter
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year', 'author__name']
@@ -30,7 +31,8 @@ class BookCreateView(generics.CreateAPIView):
     """Create new book (POST)"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  
+    permission_classes = [IsAuthenticated]
+    
     def perform_create(self, serializer):
         """Custom book creation logic"""
         serializer.save(created_by=self.request.user)
@@ -39,7 +41,8 @@ class BookUpdateView(generics.UpdateAPIView):
     """Update existing book (PUT/PATCH)"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] 
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 class BookDeleteView(generics.DestroyAPIView):
     """Delete book (DELETE)"""
     queryset = Book.objects.all()
