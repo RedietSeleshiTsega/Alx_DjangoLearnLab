d418354f7c5a7347e795335550d7ef2a137b0597
Rediet Tsega <redietseleshii@gmail.com>
2025-08-19 15:53:53 +0300

    Updated notifications/models.py.

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -5,19 +5,31 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 class Notification(models.Model):
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
-    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='acted_notifications')
-    verb = models.CharField(max_length=255)  
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='notifications'
+    )
+    actor = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='actor_notifications'
+    )
+    verb = models.CharField(max_length=255)
+    
+    content_type = models.ForeignKey(
+        ContentType, 
+        on_delete=models.CASCADE, 
+        null=True, blank=True
+    )
     object_id = models.PositiveIntegerField(null=True, blank=True)
     target = GenericForeignKey('content_type', 'object_id')
     
+    timestamp = models.DateTimeField(default=timezone.now) 
     is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=timezone.now)
     
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-timestamp']  
     
     def __str__(self):
         return f"{self.actor.username} {self.verb}"
