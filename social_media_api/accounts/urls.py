4e8e872e3f1e4a47517fe7beec6e046e7a496f4e
Rediet Tsega <redietseleshii@gmail.com>
2025-08-19 15:29:44 +0300

    Updated accounts/urls.py.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.user_login, name='login'),
     path('profile/', views.user_profile, name='profile'),
-
-    path('follow/', views.FollowUserView.as_view(), name='follow'),
-    path('unfollow/', views.UnfollowUserView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', views.FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', views.UnfollowUserView.as_view(), name='unfollow'),
     path('following/', views.FollowingListView.as_view(), name='following-list'),
     path('followers/', views.FollowersListView.as_view(), name='followers-list'),
     path('user/<int:user_id>/', views.UserProfileDetailView.as_view(), name='user-profile-detail'),
